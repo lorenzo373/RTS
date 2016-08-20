03cae95f64ffa5f357220a1819908fc1970b25ce
erkro1 <erkro1@hotmail.com>
2016-08-20 15:35:21 +0200

    Removed categories from Asset manager

--- client/js/classes/assets.js
+++ client/js/classes/assets.js
@@ -1,57 +1,26 @@
 class Assets {
 	constructor() {
 		this.ASSET_DIR = "./assets/";
-		this.CATEGORY_SPRITE = 0;
-		this.CATEGORY_TILE = 1;
 
 		//arrays for all the different categories
-		this.tile = [];
-		this.sprite = [];
+		this.textures = [];
 
 		//load all assets here
-		this.addAsset(this.CATEGORY_SPRITE, 'command', 'command.png');
+		this.addAsset('command', 'command.png');
 
 		//get asset with this
-		this.getAsset(this.CATEGORY_SPRITE, 'command');
+		this.getAsset('command');
 	}
 
-	addAsset(category, identifier, sprite) {
-		let categoryArray = [];
-
-		switch(category) {
-			case this.CATEGORY_SPRITE:
-				categoryArray = this.sprite;
-				break;
-			case this.CATEGORY_TILE:
-				categoryArray = this.tile;
-				break;
-			default:
-				console.log('Assets: invalid category');
-				break;
-		}
-
-		categoryArray.push({"identifier": identifier, "sprite": PIXI.Texture.fromImage(this.ASSET_DIR + sprite)});
+	addAsset(identifier, sprite) {
+		this.textures.push({"identifier": identifier, "sprite": PIXI.Texture.fromImage(this.ASSET_DIR + sprite)});
 	}
 
-	getAsset(category, identifier) {
-		let categoryArray = [];
-
-		switch(category) {
-			case this.CATEGORY_SPRITE:
-				categoryArray = this.sprite;
-				break;
-			case this.CATEGORY_TILE:
-				categoryArray = this.tile;
-				break;
-			default:
-				console.log('Assets: invalid category');
-				return;
-		}
-
-		for(var index = 0; index < categoryArray.length; index++) {
-			if (categoryArray[index].identifier === identifier) {
-				console.log(categoryArray[index].sprite);
-				return categoryArray[index].sprite;
+	getAsset(identifier) {
+		for(var index = 0; index < this.textures.length; index++) {
+			if (this.textures[index].identifier === identifier) {
+				console.log(this.textures[index].sprite);
+				return this.textures[index].sprite;
 			}
 		}
 
