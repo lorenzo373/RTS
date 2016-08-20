4396e896ca99fda2504cd1982013da7f155d2585
erkro1 <erkro1@hotmail.com>
2016-08-20 21:28:55 +0200

    Try catch it

--- client/js/classes/assets.js
+++ client/js/classes/assets.js
@@ -7,11 +7,11 @@ class Assets {
 	}
 
 	loadAssetsFolder(callback) {
+		try {
 		var xhttp = new XMLHttpRequest();
 
 		xhttp.onreadystatechange = function() {
 	        if (xhttp.readyState == XMLHttpRequest.DONE) {
-	        	if (xhttp.status === 200) { 
 	        	var array = xhttp.responseText.split(',');
 
 	        	for (var index = 0; index < array.length; index++) {
@@ -27,14 +27,17 @@ class Assets {
 	        	}
 
 	        	callback();
-	        	} else {
-	        		console.log(xhttp.status);
-	        	}
 	        }
     	}
 
+    	
+
 		xhttp.open("GET", "./assetloader.php", true);
 		xhttp.send();
+
+		} catch(e) {
+			console.log("error " + e);
+		}
 	}
 
 	addAsset(identifier, sprite) {
